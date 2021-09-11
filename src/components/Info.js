8d065a95d5ad7783d9dbe60eb495fd0b588d3639
enesaliovur <61868498+enesaliovur@users.noreply.github.com>
2021-09-11 12:04:43 +0300

    Uygulama Sisteme Yüklendi

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Info = (props) => {
+  return (
+  <tr>
+      <td style={{ border: "1px solid black" }}>{props.exchangeType}</td>
+      <td style={{ border: "1px solid black" }}>{props.buy}</td>
+      <td style={{ border: "1px solid black" }}>{props.sell}</td>
+      <td style={{ border: "1px solid black" }}>{props.diff}</td>
+    
+      </tr>
+  );
+};
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Title = ({ title }) => {
+  return (
+   
+      <th style={{ border: "1px solid black" }}>{title}</th>
+  
+  );
+};
